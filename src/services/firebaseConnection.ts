538e0bcde2365b489c7baacfab177e7c065fd7e5
Eduardo Soares <128731192+eduardossoares@users.noreply.github.com>
2024-10-08 19:57:32 -0300

    Update firebaseConnection.ts

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyChN0KvoHRgLM6UgHhWHyd8BBsMrjqiheI",
-  authDomain: "reactlinks-2fab0.firebaseapp.com",
-  projectId: "reactlinks-2fab0",
-  storageBucket: "reactlinks-2fab0.appspot.com",
-  messagingSenderId: "810327310096",
-  appId: "1:810327310096:web:1a1012d8f482203b9b2f95",
-  measurementId: "G-2M3ESS8WNT"
+  // Atualizard .env
 };
 
 const app = initializeApp(firebaseConfig);
